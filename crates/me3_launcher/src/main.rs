8179ad44bbeba82967acd2e8b74f384607abde2d
Gary Tierney <gary.tierney@fastmail.com>
2022-10-11 22:52:53 +0100

    Allow specifying a process name to me3_launcher

--- crates/me3_launcher/src/main.rs
+++ crates/me3_launcher/src/main.rs
@@ -4,10 +4,11 @@ use dll_syringe::{process::OwnedProcess, Syringe};
 
 fn main() {
     // TODO: parameterize
-    let target = OwnedProcess::find_first_by_name("DarkSoulsIII").expect("process not found");
+    let target_name = std::env::args().nth(1).expect("expected process name");
+    let target = OwnedProcess::find_first_by_name(&target_name).expect("process not found");
     let injector = Syringe::for_process(target);
     let dll_name = std::env::args()
-        .nth(1)
+        .nth(2)
         .unwrap_or_else(|| "me3_host.dll".to_owned());
 
     injector
