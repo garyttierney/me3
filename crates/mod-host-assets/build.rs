3ac6112d88fea6a370825d1b8f98bfaa66d1e0ad
Dasaav-dsv <SimplyDasaav@gmail.com>
2025-05-26 04:40:16 +0200

    Remove cxx build.rs

--- crates/mod-host-assets/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    cxx_build::bridge("src/lib.rs")
-        .include("include")
-        .compile("test");
-
-    println!("cargo:rerun-if-changed=src/lib.rs");
-    println!("cargo:rerun-if-changed=include/dl_allocator.hpp");
-    println!("cargo:rerun-if-changed=include/dl_string_bridge.hpp");
-}
