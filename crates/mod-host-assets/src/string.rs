d77b5e715a945575b3d74d49b649156f8a776ac4
Dasaav-dsv <SimplyDasaav@gmail.com>
2025-05-27 03:46:16 +0200

    Capitalization and implementation fixes

--- crates/mod-host-assets/src/string.rs
+++ crates/mod-host-assets/src/string.rs
@@ -1,17 +1,26 @@
 //! DLBasicString with additional type checking using the provided encoding
 //! discriminator.
 //!
-//! [`DLString`] instances can be read and written from existing structures, but not created.
+//! [`DlString`] instances can be read and written from existing structures, but not created.
 //!
 //! [`DLHashString`] instances cache its DLHash 32-bit hash using interior mutability.
 //!
 //! Thanks to Axi! for finding out the possible encoding tags.
+
+use core::fmt;
+use std::{
+    ffi::OsStr,
+    mem,
+    ops::{Deref, DerefMut},
+};
 
 use cxx_stl::{
     alloc::CxxProxy,
     string::{CxxNarrowString, CxxUtf16String, CxxUtf32String, CxxUtf8String},
 };
 use thiserror::Error;
+
+use crate::alloc::DlStdAllocator;
 
 const UTF8: u8 = 0;
 const UTF16: u8 = 1;
@@ -21,124 +30,162 @@ const EUC_JP: u8 = 4;
 const UTF32: u8 = 5;
 
 #[repr(u8)]
-#[allow(non_camel_case_types)]
 #[derive(Clone, Copy, Debug)]
-pub enum DLStringEncoding {
-    UTF8 = UTF8,
-    UTF16 = UTF16,
-    ISO_8859 = ISO_8859,
-    SJIS = SJIS,
-    EUC_JP = EUC_JP,
-    UTF32 = UTF32,
+pub enum DlCharacterSet {
+    Utf8 = UTF8,
+    Utf16 = UTF16,
+    Iso8859 = ISO_8859,
+    ShiftJis = SJIS,
+    EucJp = EUC_JP,
+    Utf32 = UTF32,
 }
 
-pub type DLStringUtf8<A> = DLString<CxxUtf8String<A>, { UTF8 }>;
+pub type DlUtf8String<A = DlStdAllocator> = DlString<CxxUtf8String<A>, { UTF8 }>;
 
-pub type DLStringUtf16<A> = DLString<CxxUtf16String<A>, { UTF16 }>;
+pub type DlUtf16String<A = DlStdAllocator> = DlString<CxxUtf16String<A>, { UTF16 }>;
 
-pub type DLStringIso8859<A> = DLString<CxxNarrowString<A>, { ISO_8859 }>;
+pub type DlIso8859String<A = DlStdAllocator> = DlString<CxxNarrowString<A>, { ISO_8859 }>;
 
-pub type DLStringSjis<A> = DLString<CxxNarrowString<A>, { SJIS }>;
+pub type DlSjisString<A = DlStdAllocator> = DlString<CxxNarrowString<A>, { SJIS }>;
 
-pub type DLStringEucJP<A> = DLString<CxxNarrowString<A>, { EUC_JP }>;
+pub type DlEucJpString<A = DlStdAllocator> = DlString<CxxNarrowString<A>, { EUC_JP }>;
 
-pub type DLStringUtf32<A> = DLString<CxxUtf32String<A>, { UTF32 }>;
+pub type DlUtf32String<A = DlStdAllocator> = DlString<CxxUtf32String<A>, { UTF32 }>;
 
+#[repr(C)]
 #[derive(Clone)]
-pub struct DLString<T, const E: u8> {
+pub struct DlString<T, const E: u8> {
     inner: T,
     encoding: u8,
 }
 
-impl<T, const E: u8> DLString<T, E> {
-    pub fn encoding(&self) -> Result<DLStringEncoding, <DLStringEncoding as TryFrom<u8>>::Error> {
+#[repr(transparent)]
+#[derive(Clone, Debug)]
+pub struct TrustedDlString<T, const E: u8>(DlString<T, E>);
+
+impl<T, const E: u8> DlString<T, E> {
+    pub fn encoding(&self) -> Result<DlCharacterSet, <DlCharacterSet as TryFrom<u8>>::Error> {
         self.encoding.try_into()
     }
 
-    pub fn get(&self) -> Result<&T, EncodingError> {
+    pub fn get(&self) -> Result<&TrustedDlString<T, E>, EncodingError> {
         if self.encoding == E {
-            Ok(&self.inner)
+            // SAFETY: transmuting into transparent wrapper.
+            unsafe { Ok(mem::transmute(self)) }
         } else {
             Err(EncodingError::new(Self::EXPECTED, self.encoding))
         }
     }
 
-    pub fn get_mut(&mut self) -> Result<&mut T, EncodingError> {
+    pub fn get_mut(&mut self) -> Result<&mut TrustedDlString<T, E>, EncodingError> {
         if self.encoding == E {
-            Ok(&mut self.inner)
+            // SAFETY: transmuting into transparent wrapper.
+            unsafe { Ok(mem::transmute(self)) }
         } else {
             Err(EncodingError::new(Self::EXPECTED, self.encoding))
         }
     }
 
-    pub unsafe fn get_unchecked(&self) -> &T {
-        &self.inner
+    /// # Safety
+    /// Only if the static encoding matches the encoding of `self`.
+    pub unsafe fn get_unchecked(&self) -> &TrustedDlString<T, E> {
+        // SAFETY: transmuting into transparent wrapper.
+        unsafe { mem::transmute(self) }
     }
 
+    /// # Safety
+    /// Only if the static encoding matches the encoding of `self`.
     pub unsafe fn get_unchecked_mut(&mut self) -> &mut T {
-        &mut self.inner
+        // SAFETY: transmuting into transparent wrapper.
+        unsafe { mem::transmute(self) }
     }
 
-    const EXPECTED: DLStringEncoding = match DLStringEncoding::from_raw(E) {
+    const EXPECTED: DlCharacterSet = match DlCharacterSet::from_raw(E) {
         Ok(encoding) => encoding,
         Err(_undefined) => panic!("encoding not defined"),
     };
 }
 
-#[cfg(windows)]
-impl<A: CxxProxy> DLStringUtf16<A> {
-    pub fn decode(&self) -> Result<String, EncodingError> {
-        use std::{ffi::OsString, os::windows::ffi::OsStringExt};
-
-        Ok(OsString::from_wide(self.get()?.as_bytes())
-            .to_string_lossy()
-            .into_owned())
-    }
-
-    pub fn encode<T: AsRef<str>>(&mut self, s: T) -> Result<(), EncodingError> {
-        let inner = self.get_mut()?;
-
-        inner.clear();
-        inner.extend(s.as_ref().encode_utf16());
-
-        Ok(())
-    }
-}
-
 #[derive(Error, Debug)]
 #[error(
-    "DLString encoding error; expected {:?} but got {:?}",
-    expected, DLStringEncoding::try_from(*actual)
+    "DlString encoding error; expected {:?} but got {:?}",
+    expected, DlCharacterSet::try_from(*actual)
 )]
 pub struct EncodingError {
-    expected: DLStringEncoding,
+    expected: DlCharacterSet,
     actual: u8,
 }
 
 impl EncodingError {
-    const fn new(expected: DLStringEncoding, actual: u8) -> Self {
+    const fn new(expected: DlCharacterSet, actual: u8) -> Self {
         Self { expected, actual }
     }
 }
 
-impl DLStringEncoding {
+impl DlCharacterSet {
     const fn from_raw(value: u8) -> Result<Self, u8> {
         match value {
-            UTF8 => Ok(DLStringEncoding::UTF8),
-            UTF16 => Ok(DLStringEncoding::UTF16),
-            ISO_8859 => Ok(DLStringEncoding::ISO_8859),
-            SJIS => Ok(DLStringEncoding::SJIS),
-            EUC_JP => Ok(DLStringEncoding::EUC_JP),
-            UTF32 => Ok(DLStringEncoding::UTF32),
+            UTF8 => Ok(DlCharacterSet::Utf8),
+            UTF16 => Ok(DlCharacterSet::Utf16),
+            ISO_8859 => Ok(DlCharacterSet::Iso8859),
+            SJIS => Ok(DlCharacterSet::ShiftJis),
+            EUC_JP => Ok(DlCharacterSet::EucJp),
+            UTF32 => Ok(DlCharacterSet::Utf32),
             value => Err(value),
         }
     }
 }
 
-impl TryFrom<u8> for DLStringEncoding {
+impl TryFrom<u8> for DlCharacterSet {
     type Error = u8;
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         Self::from_raw(value)
+    }
+}
+
+impl<T, const E: u8> Deref for TrustedDlString<T, E> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0.inner
+    }
+}
+
+impl<T, const E: u8> DerefMut for TrustedDlString<T, E> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0.inner
+    }
+}
+
+impl<A: CxxProxy> ToString for TrustedDlString<CxxUtf8String<A>, { UTF8 }> {
+    fn to_string(&self) -> String {
+        // SAFETY: `self` is encoded at least as a UTF-8 superset
+        // as verified by checking the encoding.
+        unsafe {
+            OsStr::from_encoded_bytes_unchecked(self.as_bytes())
+                .to_string_lossy()
+                .into_owned()
+        }
+    }
+}
+
+#[cfg(windows)]
+impl<A: CxxProxy> ToString for TrustedDlString<CxxUtf16String<A>, { UTF16 }> {
+    fn to_string(&self) -> String {
+        use std::{ffi::OsString, os::windows::ffi::OsStringExt};
+
+        OsString::from_wide(self.as_bytes())
+            .to_string_lossy()
+            .into_owned()
+    }
+}
+
+impl<T: fmt::Debug, const E: u8> fmt::Debug for DlString<T, E> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("DlString")
+            .field("inner", &self.inner)
+            .field("encoding", &self.encoding())
+            .finish()
     }
 }
