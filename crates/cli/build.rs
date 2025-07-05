79f86e318acc242f6314f54484636551b3459316
Dasaav-dsv <SimplyDasaav@gmail.com>
2025-07-05 19:20:23 +0200

    chore(dev): comment out winresource

--- crates/cli/build.rs
+++ crates/cli/build.rs
@@ -1,8 +1,8 @@
 fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        let mut res = winresource::WindowsResource::new();
-        res.set_manifest_file("resources/manifest.xml");
-        res.set_icon("resources/me3.ico");
-        res.compile().unwrap();
-    }
+    // if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+    //     let mut res = winresource::WindowsResource::new();
+    //     res.set_manifest_file("resources/manifest.xml");
+    //     res.set_icon("resources/me3.ico");
+    //     res.compile().unwrap();
+    // }
 }
