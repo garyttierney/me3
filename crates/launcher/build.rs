a41c0f5b8e3fbdfa06501e89e83854ca6f1bf1e3
Dasaav-dsv <SimplyDasaav@gmail.com>
2025-07-05 19:11:54 +0200

    chore(dev): try removing winresource (except from cli)

--- crates/launcher/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        let mut res = winresource::WindowsResource::new();
-        res.compile().unwrap();
-    }
-}
