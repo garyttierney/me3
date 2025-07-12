2658e5f033e2e0c80df0a3d0045c4e0a2ab9a3a4
Justin Koh <j@ustink.org>
2025-07-12 11:45:33 +0800

    Clean up comments

--- distribution/linux/install-user.sh
+++ distribution/linux/install-user.sh
@@ -10,7 +10,6 @@ install -Dpm 0755 -t "${bindir}" bin/me3
 install -Dpm 0644 -t "${datadir}/me3/windows-bin" bin/win64/me3-launcher.exe \
                                                   bin/win64/me3_mod_host.dll
 
-# assuming these files are in /dist inside the tarball
 install -Dpm 0644 -t "${datadir}/applications" dist/me3-launch.desktop
 install -Dpm 0644 -t "${datadir}/mime/packages" dist/me3.xml
 install -Dpm 0644 -t "${datadir}/icons/hicolor/128x128/apps" dist/me3.png
@@ -24,7 +23,7 @@ windows_binaries_dir = "${datadir}/me3/windows-bin"
 EOF
 fi
 
-# install example profiles?
+# install example profiles
 if [ ! -d "${confdir}/me3/profiles" ]; then
     install -Dpm 0644 -t "${confdir}/me3/profiles" ./*.me3
     mkdir "${confdir}/me3/profiles/eldenring-mods"
