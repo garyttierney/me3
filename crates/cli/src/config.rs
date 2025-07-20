32970316552c3657efb548ec76138bc09b1304a4
Gary Tierney <gary.tierney@fastmail.com>
2025-07-20 16:54:28 +0100

    chore: fmt

--- crates/cli/src/config.rs
+++ crates/cli/src/config.rs
@@ -98,11 +98,7 @@ impl Options {
     pub fn merge(self, other: Self) -> Self {
         Self {
             crash_reporting: other.crash_reporting.or(self.crash_reporting),
-            game: other
-                .game
-                .into_iter()
-                .chain(self.game)
-                .collect(),
+            game: other.game.into_iter().chain(self.game).collect(),
             profile_dir: other.profile_dir.or(self.profile_dir),
             steam_dir: other.steam_dir.or(self.steam_dir),
             windows_binaries_dir: other.windows_binaries_dir.or(self.windows_binaries_dir),
