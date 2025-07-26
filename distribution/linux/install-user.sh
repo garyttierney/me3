30664839c273cd005879dbbf95afeddb043e595c
jn64 <23169302+jn64@users.noreply.github.com>
2025-07-26 08:26:30 +0800

    fix(linux): Don't create default config (#379)
    Redundant after #336

--- distribution/linux/install-user.sh
+++ distribution/linux/install-user.sh
@@ -14,14 +14,6 @@ install -Dpm 0644 -t "${datadir}/applications" dist/me3-launch.desktop
 install -Dpm 0644 -t "${datadir}/mime/packages" dist/me3.xml
 install -Dpm 0644 -t "${datadir}/icons/hicolor/128x128/apps" dist/me3.png
 
-if [ ! -f "${confdir}/me3/me3.toml" ]; then
-    mkdir -p "${confdir}/me3"
-    cat >"${confdir}/me3/me3.toml" <<EOF
-crash_reporting = false
-windows_binaries_dir = "${datadir}/me3/windows-bin"
-EOF
-fi
-
 # install example profiles
 if [ ! -d "${confdir}/me3/profiles" ]; then
     install -Dpm 0644 -t "${confdir}/me3/profiles" ./*.me3
