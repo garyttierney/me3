295f6aad1c08fbccb978b0ccd4188ada44d16c69
Dasaav-dsv <simplydasaav@gmail.com>
2025-12-24 16:25:56 +0100

    feat(host): Log game version on attach
    Signed-off-by: Dasaav-dsv <simplydasaav@gmail.com>

--- crates/mod-host/src/executable.rs
+++ crates/mod-host/src/executable.rs
@@ -1,11 +1,30 @@
-use std::{cell::UnsafeCell, slice};
+use std::{borrow::Cow, cell::UnsafeCell, fmt, slice};
 
-use pelite::pe::{Pe, PeObject, PeView};
+use pelite::{
+    pe::{Pe, PeObject, PeView},
+    resources::FindError,
+};
 use windows::{core::PCWSTR, Win32::System::LibraryLoader::GetModuleHandleW};
 
 #[derive(Clone, Copy)]
 pub struct Executable {
     inner: &'static UnsafeCell<[u8]>,
+}
+
+#[derive(Clone, Debug)]
+pub struct Version {
+    pub product: Cow<'static, str>,
+    pub major: u16,
+    pub minor: u16,
+    pub patch: u16,
+    pub build: u16,
+    pub region: Region,
+}
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum Region {
+    Worldwide,
+    Japan,
 }
 
 impl Executable {
@@ -30,6 +49,44 @@ impl Executable {
             }
         }
     }
+
+    pub fn version(&self) -> Result<Version, FindError> {
+        let version_info = self.resources()?.version_info()?;
+        let product_version = version_info
+            .fixed()
+            .ok_or(FindError::NotFound)?
+            .dwProductVersion;
+
+        let language = version_info.translation().first();
+
+        let product = language.and_then(|language| {
+            let mut product = None;
+            version_info.strings(*language, |key, value| {
+                (key == "ProductName").then(|| product = Some(Cow::Owned(value.to_owned())));
+            });
+            product
+        });
+
+        let product = product.unwrap_or(Cow::Borrowed("Unknown Product"));
+
+        // Use FROMSOFTWARE version mapping scheme:
+        let major = product_version.Major.min(1);
+        let minor = product_version.Minor + (product_version.Major - major) * 10;
+
+        let region = match language {
+            Some(language) if language.lang_id == 0x411 => Region::Japan,
+            _ => Region::Worldwide,
+        };
+
+        Ok(Version {
+            product,
+            major,
+            minor,
+            patch: product_version.Patch,
+            build: product_version.Build,
+            region,
+        })
+    }
 }
 
 unsafe impl PeObject<'static> for Executable {
@@ -43,6 +100,22 @@ unsafe impl PeObject<'static> for Executable {
 }
 
 unsafe impl Pe<'static> for Executable {}
+
+impl fmt::Display for Version {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} {}.{:02}.{}.{} {}",
+            self.product, self.major, self.minor, self.patch, self.build, self.region
+        )
+    }
+}
+
+impl fmt::Display for Region {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
 
 unsafe impl Send for Executable {}
 
