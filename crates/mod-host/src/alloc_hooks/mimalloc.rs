2e39640395127d2d47313e52494b7c775b88920d
Dasaav-dsv <simplydasaav@gmail.com>
2026-07-12 16:03:00 +0200

    fix(host): Improve mem_patch stability
    Signed-off-by: Dasaav-dsv <simplydasaav@gmail.com>

--- crates/mod-host/src/alloc_hooks/mimalloc.rs
+++ crates/mod-host/src/alloc_hooks/mimalloc.rs
@@ -1,7 +1,14 @@
-use std::{mem::ManuallyDrop, ptr::NonNull};
+use std::{mem::ManuallyDrop, ptr::NonNull, sync::LazyLock};
 
-use libmimalloc_sys::{mi_free, mi_malloc_aligned, mi_realloc_aligned, mi_usable_size};
-use me3_mod_host_types::alloc::{DlAllocator, DlAllocatorVtable, DlHeapDirection};
+use libmimalloc_sys::{
+    mi_arena_id_t, mi_free, mi_heap_malloc_aligned, mi_heap_new_in_arena, mi_heap_realloc_aligned,
+    mi_heap_t, mi_option_set, mi_reserve_os_memory_ex, mi_usable_size,
+};
+use me3_mod_host_types::{
+    alloc::{DlAllocator, DlAllocatorVtable, DlHeapDirection},
+    game::GAME,
+};
+use me3_mod_protocol::Game;
 
 pub static MIMALLOC_DLALLOC: DlAllocator = DlAllocator {
     vtable: NonNull::from_ref(&MIMALLOC_DLALLOC_VTABLE),
@@ -34,6 +41,39 @@ const MIMALLOC_DLALLOC_VTABLE: DlAllocatorVtable = DlAllocatorVtable {
     unlock,
     block_of,
 };
+
+/// A contiguous, pre-committed heap used in place of mimalloc's default heap to reduce
+/// crashes caused by bad memory accesses.
+static mut MI_HEAP: LazyLock<*mut mi_heap_t> = LazyLock::new(|| unsafe {
+    // Disable decommitting purged pages to reduce the likelihood of crashing
+    // because of use-after-free and OOB access bugs. This is especially important in Sekiro,
+    // which consistently causes OOB reads in a certain `GXFlverMaterial` function (these reads
+    // may cross page boundaries and segfault on a decommitted page when this option is "1").
+    let mi_option_purge_decommits = 5;
+    mi_option_set(mi_option_purge_decommits, 0);
+
+    // Assuming an overcommit system, numbers are based on how much memory the games already
+    // commit, manual testing and expectations for upper bounds on memory usage.
+    // Since `mi_option_disallow_os_alloc` is not set mimalloc may reserve even more memory
+    // outside of this arena as it needs.
+    let size_mb = match *GAME {
+        Game::DarkSouls3 => 6 * 1024,
+        Game::Sekiro => 6 * 1024,
+        Game::EldenRing => 12 * 1024,
+        _ => unimplemented!("this game does not support mem_patch?"),
+    };
+
+    let size_bytes = size_mb * 1024 * 1024;
+    let mut arena_id = mi_arena_id_t::default();
+
+    let res = mi_reserve_os_memory_ex(size_bytes, true, true, true, &mut arena_id);
+
+    if res != 0 {
+        panic!("mimalloc failed to reserve and commit OS memory");
+    }
+
+    mi_heap_new_in_arena(arena_id)
+});
 
 unsafe extern "C" fn dtor(_: NonNull<ManuallyDrop<DlAllocator>>) {}
 
@@ -80,27 +120,27 @@ unsafe extern "C" fn block_size(_: NonNull<DlAllocator>, block: *mut u8) -> usiz
     }
 }
 
-#[inline]
-unsafe extern "C" fn allocate(_: NonNull<DlAllocator>, size: usize) -> *mut u8 {
-    unsafe { mi_malloc_aligned(size.next_multiple_of(16), 16) as _ }
+unsafe extern "C" fn allocate(this: NonNull<DlAllocator>, size: usize) -> *mut u8 {
+    unsafe { allocate_aligned(this, size, 16) }
 }
 
-#[inline]
 unsafe extern "C" fn allocate_aligned(
     _: NonNull<DlAllocator>,
     size: usize,
     alignment: usize,
 ) -> *mut u8 {
     let alignment = alignment.max(16);
-    unsafe { mi_malloc_aligned(size.next_multiple_of(alignment), alignment) as _ }
+    unsafe { mi_heap_malloc_aligned(*MI_HEAP, size.next_multiple_of(alignment), alignment) as _ }
 }
 
-#[inline]
-unsafe extern "C" fn reallocate(_: NonNull<DlAllocator>, old: *mut u8, new_size: usize) -> *mut u8 {
-    unsafe { mi_realloc_aligned(old as _, new_size.next_multiple_of(16), 16) as _ }
+unsafe extern "C" fn reallocate(
+    this: NonNull<DlAllocator>,
+    old: *mut u8,
+    new_size: usize,
+) -> *mut u8 {
+    unsafe { reallocate_aligned(this, old, new_size, 16) }
 }
 
-#[inline]
 unsafe extern "C" fn reallocate_aligned(
     _: NonNull<DlAllocator>,
     old: *mut u8,
@@ -108,10 +148,16 @@ unsafe extern "C" fn reallocate_aligned(
     alignment: usize,
 ) -> *mut u8 {
     let alignment = alignment.max(16);
-    unsafe { mi_realloc_aligned(old as _, new_size.next_multiple_of(alignment), alignment) as _ }
+    unsafe {
+        mi_heap_realloc_aligned(
+            *MI_HEAP,
+            old as _,
+            new_size.next_multiple_of(alignment),
+            alignment,
+        ) as _
+    }
 }
 
-#[inline]
 unsafe extern "C" fn free(_: NonNull<DlAllocator>, ptr: *mut u8) {
     unsafe {
         mi_free(ptr as _);
